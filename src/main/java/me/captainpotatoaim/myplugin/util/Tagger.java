9698c3c0288bd8347238c7428be64c79f893a135
anviks <anviks@ttu.ee>
2024-09-13 15:12:00 +0300

    Rename methods to be more general

--- src/main/java/me/captainpotatoaim/myplugin/util/Tagger.java
+++ src/main/java/me/captainpotatoaim/myplugin/util/Tagger.java
@@ -2,63 +2,37 @@ package me.captainpotatoaim.myplugin.util;
 
 import me.captainpotatoaim.myplugin.Initializer;
 import org.bukkit.NamespacedKey;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
-import org.apache.commons.codec.binary.Hex;
-
-import java.security.SecureRandom;
 
 public class Tagger {
-    public static final NamespacedKey KEY = new NamespacedKey(Initializer.plugin,
-            "custom-item");
+    public static final NamespacedKey CUSTOM_ITEM_KEY = new NamespacedKey(Initializer.plugin, "custom_item_type");
 
     public static boolean hasIdentifier(ItemStack item, String identifier) {
         var meta = item.getItemMeta();
-        assert meta != null;
+        if (meta == null) return false;
         var container = meta.getPersistentDataContainer();
-        var itemIdentifier = container.get(KEY, PersistentDataType.STRING);
+        var itemIdentifier = container.get(CUSTOM_ITEM_KEY, PersistentDataType.STRING);
         return itemIdentifier != null && itemIdentifier.equals(identifier);
     }
 
     public static boolean hasIdentifier(Entity entity, String identifier) {
         var container = entity.getPersistentDataContainer();
-        var itemIdentifier = container.get(KEY, PersistentDataType.STRING);
+        var itemIdentifier = container.get(CUSTOM_ITEM_KEY, PersistentDataType.STRING);
         return itemIdentifier != null && itemIdentifier.equals(identifier);
     }
 
-    public static void tagItem(ItemStack item, String identifier) {
+    public static void addIdentifier(ItemStack item, String identifier) {
         var meta = item.getItemMeta();
         assert meta != null;
         var container = meta.getPersistentDataContainer();
-        container.set(KEY, PersistentDataType.STRING, identifier);
+        container.set(CUSTOM_ITEM_KEY, PersistentDataType.STRING, identifier);
         item.setItemMeta(meta);
     }
 
-    public static void tagEntity(Entity entity, String identifier) {
+    public static void addIdentifier(Entity entity, String identifier) {
         var container = entity.getPersistentDataContainer();
-        container.set(KEY, PersistentDataType.STRING, identifier);
-    }
-
-    private static String generateIdentifier() {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] randomBytes = new byte[32];
-        secureRandom.nextBytes(randomBytes);
-
-        return new String(Hex.encodeHex(randomBytes));
-    }
-
-    public static String getIdentifier(String item) {
-        FileConfiguration config = Initializer.plugin.getConfig();
-        String identifier = config.getString("identifiers." + item);
-
-        if (identifier == null || identifier.length() != 64) {
-            identifier = generateIdentifier();
-            config.set("identifiers." + item, identifier);
-            Initializer.plugin.saveConfig();
-        }
-
-        return identifier;
+        container.set(CUSTOM_ITEM_KEY, PersistentDataType.STRING, identifier);
     }
 }
