b926e4f83ac018d7b70b836a212b7e2ffb725f2f
anviks <anviks@ttu.ee>
2024-09-13 20:53:31 +0300

    Refactor code and call EntityShootBowEvent after every time the rapid bow fires

--- src/main/java/me/captainpotatoaim/myplugin/custom_items/rapid_fire_bow/RapidFireBowListener.java
+++ src/main/java/me/captainpotatoaim/myplugin/custom_items/rapid_fire_bow/RapidFireBowListener.java
@@ -1,9 +1,9 @@
 package me.captainpotatoaim.myplugin.custom_items.rapid_fire_bow;
 
-import com.google.gson.Gson;
 import me.captainpotatoaim.myplugin.Initializer;
 import me.captainpotatoaim.myplugin.custom_items.CustomItem;
 import me.captainpotatoaim.myplugin.util.Tagger;
+import net.kyori.adventure.text.Component;
 import org.bukkit.*;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
@@ -12,7 +12,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
@@ -24,51 +23,48 @@ import org.bukkit.util.Vector;
 import java.util.*;
 
 public class RapidFireBowListener implements Listener {
-    private final Map<UUID, Integer> shootingPlayers = new HashMap<>();
+    private final Map<UUID, BukkitTask> shootingPlayers = new HashMap<>();
 
     @EventHandler
     void onBowShoot(EntityShootBowEvent event) {
-        if (CustomItem.isOfType(event.getBow(), RapidFireBow.class) && event.getEntity() instanceof Player player) {
+        if (CustomItem.isOfType(event.getBow(), RapidFireBow.class)
+                && event.getEntity() instanceof Player player
+                && !shootingPlayers.containsKey(player.getUniqueId())) {
             shootArrows(player, event);
         }
     }
 
     @EventHandler
     void onPlayerLeave(PlayerQuitEvent event) {
-        stopShooting(event.getPlayer().getUniqueId());
+        stopShooting(event.getPlayer());
     }
 
     @EventHandler
     void onClick(PlayerInteractEvent event) {
         Action action = event.getAction();
         if (!action.equals(Action.PHYSICAL)) {
-            stopShooting(event.getPlayer().getUniqueId());
+            stopShooting(event.getPlayer());
         }
     }
 
     @EventHandler
     void onPlayerDeath(PlayerDeathEvent event) {
-        stopShooting(event.getEntity().getUniqueId());
+        stopShooting(event.getEntity());
     }
 
     @EventHandler
     void onItemChange(PlayerItemHeldEvent event) {
-        stopShooting(event.getPlayer().getUniqueId());
+        stopShooting(event.getPlayer());
     }
 
-    private void stopShooting(UUID uuid) {
-        Integer taskId = shootingPlayers.remove(uuid);
-        if (taskId != null) {
-            Bukkit.getScheduler().cancelTask(taskId);
-        }
+    private void stopShooting(Player player) {
+        var task = shootingPlayers.remove(player.getUniqueId());
+        if (task != null) task.cancel();
     }
 
     private void shootArrows(Player player, EntityShootBowEvent event) {
         ItemStack itemArrow = event.getConsumable();
-
-        if (itemArrow == null) {
-            return;
-        }
+        if (itemArrow == null) return;
 
         Material material = itemArrow.getType();
         Class<? extends AbstractArrow> arrowClass;
@@ -88,11 +84,16 @@ public class RapidFireBowListener implements Listener {
         assert bow != null;
 
         Vector arrowDirection = event.getProjectile().getVelocity();
-        Runnable shoot = () -> shootArrowTask(player, arrowDirection, bow, itemArrow, identifier, arrowClass);
+        Runnable shoot = () -> shootArrowTask(player, arrowDirection, bow, itemArrow, identifier, arrowClass, event);
 
-        // TODO: Change to runTaskLater when each shot begins to call bow event
-        BukkitTask task = Bukkit.getScheduler().runTaskTimer(Initializer.plugin, shoot, 0, 1);
-        shootingPlayers.put(player.getUniqueId(), task.getTaskId());
+        BukkitTask task = Bukkit.getScheduler().runTaskTimer(Initializer.plugin, shoot, 10, 10);
+        shootingPlayers.put(player.getUniqueId(), task);
+    }
+
+    @EventHandler
+    public void dmg(PlayerItemDamageEvent event) {
+        Bukkit.broadcast(Component.text(event.getDamage()));
+        Bukkit.broadcast(Component.text(event.getOriginalDamage()));
     }
 
     private void shootArrowTask(
@@ -101,23 +102,36 @@ public class RapidFireBowListener implements Listener {
             ItemStack bow,
             ItemStack arrows,
             String identifier,
-            Class<? extends Projectile> arrowClass
+            Class<? extends Projectile> arrowClass,
+            EntityShootBowEvent originalEvent
     ) {
         if (!player.getInventory().containsAtLeast(arrows, 1)) {
-            stopShooting(player.getUniqueId());
+            stopShooting(player);
             return;
         }
 
         changeArrowDirection(arrowDirection, player);
-        Projectile arrow = player.launchProjectile(arrowClass, arrowDirection);
         applyRandomOffset(arrowDirection);
-        arrow.setVelocity(arrowDirection);
+        Projectile arrow = player.launchProjectile(arrowClass, arrowDirection);
 
         if (identifier != null) {
             Tagger.addIdentifier(arrow, identifier);
         }
 
         AbstractArrow abstractArrow = (AbstractArrow) arrow;
+
+        Bukkit.getScheduler().runTaskLater(Initializer.plugin, () -> {
+            EntityShootBowEvent event1 = new EntityShootBowEvent(
+                    player,
+                    bow,
+                    arrows,
+                    abstractArrow,
+                    originalEvent.getHand(),
+                    originalEvent.getForce(),
+                    originalEvent.shouldConsumeItem()
+            );
+            Bukkit.getPluginManager().callEvent(event1);
+        }, 1);
 
         if (player.getGameMode() == GameMode.CREATIVE) {
             abstractArrow.setPickupStatus(AbstractArrow.PickupStatus.CREATIVE_ONLY);
@@ -138,7 +152,7 @@ public class RapidFireBowListener implements Listener {
             damageBow(player, bow);
         }
 
-        PlayerItemDamageEvent event = new PlayerItemDamageEvent(player, bow, damage);
+        PlayerItemDamageEvent event = new PlayerItemDamageEvent(player, bow, damage, 1);
         Bukkit.getPluginManager().callEvent(event);
     }
 
@@ -170,7 +184,7 @@ public class RapidFireBowListener implements Listener {
 
         if (bow.getType().getMaxDurability() - bowMeta.getDamage() <= 0) {
             player.getInventory().removeItem(bow);
-            stopShooting(player.getUniqueId());
+            stopShooting(player);
             float pitch = (float) (Math.random() * 0.4 + 0.8);
             player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1, pitch);
             Location eyes = player.getEyeLocation();
