b6f4b5f1d43bc12efdb58f3f386a02cc8ef1fb86
anviks <anviks@ttu.ee>
2024-09-20 17:21:12 +0300

    Rewrite Vanish class in kotlin

--- src/main/java/me/captainpotatoaim/myplugin/vanish/Vanish.kt
+++ src/main/java/me/captainpotatoaim/myplugin/vanish/Vanish.kt
@@ -1,134 +1,131 @@
-package me.captainpotatoaim.myplugin.vanish;
+package me.captainpotatoaim.myplugin.vanish
 
-import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.CommandPermission;
-import dev.jorel.commandapi.executors.CommandArguments;
-import me.captainpotatoaim.myplugin.Initializer;
-import me.captainpotatoaim.myplugin.custom_items.CustomItem;
-import me.captainpotatoaim.myplugin.util.CollectionsHelper;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoRemovePacket;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
-import net.minecraft.server.level.ServerPlayer;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.persistence.PersistentDataType;
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.CommandPermission
+import dev.jorel.commandapi.executors.CommandArguments
+import me.captainpotatoaim.myplugin.Initializer
+import me.captainpotatoaim.myplugin.custom_items.CustomItem
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoRemovePacket
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Particle
+import org.bukkit.craftbukkit.entity.CraftPlayer
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.persistence.PersistentDataType
+import java.util.*
 
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Optional;
-
-public class Vanish implements Listener {
+class Vanish : Listener {
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        Optional<Boolean> isVanished = CustomItem.getData(player, "vanished", PersistentDataType.BOOLEAN);
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        val isVanished = CustomItem.getData(player, "vanished", PersistentDataType.BOOLEAN)
 
-        if (isVanished.isPresent() && isVanished.get()) {
-            this.updatePlayerVisibilityForAll(player, false);
+        if (isVanished.isPresent && isVanished.get()) {
+            this.updatePlayerVisibilityForAll(player, false)
         }
 
-        for (Player onlinePlayer : CollectionsHelper.allExcept(Bukkit.getOnlinePlayers(), player)) {
-            Optional<Boolean> isSomeoneElseVanished = CustomItem.getData(onlinePlayer, "vanished", PersistentDataType.BOOLEAN);
+        for (onlinePlayer in Bukkit.getOnlinePlayers().minus(player)) {
+            val isSomeoneElseVanished = CustomItem.getData(onlinePlayer, "vanished", PersistentDataType.BOOLEAN)
 
-            if (isSomeoneElseVanished.isPresent() && isSomeoneElseVanished.get()) {
-                this.updatePlayerVisibility(onlinePlayer, player, false);
+            if (isSomeoneElseVanished.isPresent && isSomeoneElseVanished.get()) {
+                this.updatePlayerVisibility(onlinePlayer, player, false)
             }
         }
     }
 
-    public void registerCommand() {
-        new CommandAPICommand("vanish")
-                .withPermission(CommandPermission.OP)
-                .executesPlayer(this::run)
-                .register(Initializer.getPlugin());
+    fun registerCommand() {
+        CommandAPICommand("vanish")
+            .withPermission(CommandPermission.OP)
+            .executesPlayer(this::run)
+            .register(Initializer.getPlugin())
     }
 
-    private ClientboundPlayerInfoRemovePacket getTabListRemovePacket(Player player) {
-        return new ClientboundPlayerInfoRemovePacket(List.of(player.getUniqueId()));
+    private fun run(player: Player, args: CommandArguments) {
+        val location = player.location
+        val isVanished = CustomItem.getData(player, "vanished", PersistentDataType.BOOLEAN)
+
+        if (isVanished.isPresent && isVanished.get()) {
+            val lightningLocations = ArrayList<Location>()
+
+            for (i in -1..1) {
+                for (j in -1..1) {
+                    val lightningLocation = location.clone()
+                    lightningLocation.y -= 1
+                    lightningLocation.x += i
+                    lightningLocation.z += j
+                    lightningLocations.add(lightningLocation)
+                }
+            }
+
+            var delay = 0
+            val scheduler = Bukkit.getScheduler()
+
+            for (i in listOf(0, 1, 2, 5, 8, 7, 6, 3)) {
+                scheduler.runTaskLater(
+                    Initializer.getPlugin(),
+                    Runnable { player.world.strikeLightningEffect(lightningLocations[i]) }, delay.toLong()
+                )
+                delay += 5
+            }
+
+            delay += 15
+
+            scheduler.runTaskLater(Initializer.getPlugin(), Runnable {
+                for (lightningLocation in lightningLocations) {
+                    player.world.strikeLightningEffect(lightningLocation)
+                }
+                this.updatePlayerVisibilityForAll(player, true)
+            }, delay.toLong())
+        } else {
+            this.updatePlayerVisibilityForAll(player, false)
+            location.y += 1
+            player.world.spawnParticle(Particle.LARGE_SMOKE, location, 250, 0.5, 0.5, 0.5, 0.1)
+        }
     }
 
-    private ClientboundPlayerInfoUpdatePacket getTabListAddPacket(Player player) {
-        ServerPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+    private fun updatePlayerVisibilityForAll(player: Player, show: Boolean) {
+        CustomItem.setData(player, "vanished", PersistentDataType.BOOLEAN, !show)
 
-        var actions = EnumSet.of(
-                ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER,
-                ClientboundPlayerInfoUpdatePacket.Action.UPDATE_DISPLAY_NAME,
-                ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LATENCY,
-                ClientboundPlayerInfoUpdatePacket.Action.UPDATE_GAME_MODE,
-                ClientboundPlayerInfoUpdatePacket.Action.INITIALIZE_CHAT,
-                ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LISTED
-        );
-
-        return new ClientboundPlayerInfoUpdatePacket(actions, List.of(nmsPlayer));
+        for (onlinePlayer in Bukkit.getOnlinePlayers().minus(player)) {
+            updatePlayerVisibility(player, onlinePlayer, show)
+        }
     }
 
-    private void updatePlayerVisibility(Player target, Player observer, boolean show) {
-        var plugin = Initializer.getPlugin();
-        var packet = show ? getTabListAddPacket(target) : getTabListRemovePacket(target);
+    private fun updatePlayerVisibility(target: Player, observer: Player, show: Boolean) {
+        val plugin = Initializer.getPlugin()
+        val packet = if (show) getTabListAddPacket(target) else getTabListRemovePacket(target)
 
-        ServerPlayer nmsObserver = ((CraftPlayer) observer).getHandle();
-        nmsObserver.connection.sendPacket(packet);
+        val nmsObserver = (observer as CraftPlayer).handle
+        nmsObserver.connection.sendPacket(packet)
 
         if (show) {
-            observer.showPlayer(plugin, target);
+            observer.showPlayer(plugin, target)
         } else {
-            observer.hidePlayer(plugin, target);
+            observer.hidePlayer(plugin, target)
         }
     }
 
-    private void updatePlayerVisibilityForAll(Player player, boolean show) {
-        CustomItem.setData(player, "vanished", PersistentDataType.BOOLEAN, !show);
-
-        for (Player onlinePlayer : CollectionsHelper.allExcept(Bukkit.getOnlinePlayers(), player)) {
-            updatePlayerVisibility(player, onlinePlayer, show);
-        }
+    private fun getTabListRemovePacket(player: Player): ClientboundPlayerInfoRemovePacket {
+        return ClientboundPlayerInfoRemovePacket(listOf(player.uniqueId))
     }
 
-    private void run(Player player, CommandArguments args) {
-        Location location = player.getLocation();
-        Optional<Boolean> isVanished = CustomItem.getData(player, "vanished", PersistentDataType.BOOLEAN);
+    private fun getTabListAddPacket(player: Player): ClientboundPlayerInfoUpdatePacket {
+        val nmsPlayer = (player as CraftPlayer).handle
 
-        if (isVanished.isPresent() && isVanished.get()) {
-            var lightningLocations = new ArrayList<Location>();
+        val actions = EnumSet.of(
+            ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER,
+            ClientboundPlayerInfoUpdatePacket.Action.UPDATE_DISPLAY_NAME,
+            ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LATENCY,
+            ClientboundPlayerInfoUpdatePacket.Action.UPDATE_GAME_MODE,
+            ClientboundPlayerInfoUpdatePacket.Action.INITIALIZE_CHAT,
+            ClientboundPlayerInfoUpdatePacket.Action.UPDATE_LISTED
+        )
 
-            for (int i = -1; i < 2; i++) {
-                for (int j = -1; j < 2; j++) {
-                    Location lightningLocation = location.clone();
-                    lightningLocation.setY(lightningLocation.getY() - 1);
-                    lightningLocation.setX(lightningLocation.getX() + i);
-                    lightningLocation.setZ(lightningLocation.getZ() + j);
-                    lightningLocations.add(lightningLocation);
-                }
-            }
-
-            int delay = 0;
-            var scheduler = Bukkit.getScheduler();
-
-            for (int i : List.of(0, 1, 2, 5, 8, 7, 6, 3)) {
-                scheduler.runTaskLater(Initializer.getPlugin(), () -> player.getWorld().strikeLightningEffect(lightningLocations.get(i)), delay);
-                delay += 5;
-            }
-
-            delay += 15;
-
-            scheduler.runTaskLater(Initializer.getPlugin(), () -> {
-                for (var lightningLocation : lightningLocations) {
-                    player.getWorld().strikeLightningEffect(lightningLocation);
-                }
-                this.updatePlayerVisibilityForAll(player, true);
-            }, delay);
-        } else {
-            this.updatePlayerVisibilityForAll(player, false);
-            location.setY(location.getY() + 1);
-            player.getWorld().spawnParticle(Particle.LARGE_SMOKE, location, 250, 0.5, 0.5, 0.5, 0.1);
-        }
+        return ClientboundPlayerInfoUpdatePacket(actions, listOf(nmsPlayer))
     }
 }
