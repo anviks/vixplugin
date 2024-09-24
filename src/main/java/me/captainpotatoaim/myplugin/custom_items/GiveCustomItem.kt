31a17db5c7efa3ade7dd50c64bd224bbc0a99c2a
anviks <anviks@ttu.ee>
2024-09-24 15:39:41 +0300

    Replace suggestions with MultiLiteralArgument to have both suggestions and validation

--- src/main/java/me/captainpotatoaim/myplugin/custom_items/GiveCustomItem.kt
+++ src/main/java/me/captainpotatoaim/myplugin/custom_items/GiveCustomItem.kt
@@ -1,28 +1,24 @@
 package me.captainpotatoaim.myplugin.custom_items
 
-import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.CommandPermission
-import dev.jorel.commandapi.SuggestionInfo
-import dev.jorel.commandapi.arguments.EntitySelectorArgument.ManyPlayers
+import dev.jorel.commandapi.arguments.EntitySelectorArgument
 import dev.jorel.commandapi.arguments.FloatArgument
 import dev.jorel.commandapi.arguments.IntegerArgument
 import dev.jorel.commandapi.arguments.LiteralArgument
-import dev.jorel.commandapi.arguments.StringArgument
+import dev.jorel.commandapi.arguments.MultiLiteralArgument
 import dev.jorel.commandapi.executors.CommandArguments
-import dev.jorel.commandapi.executors.CommandExecutor
 import me.captainpotatoaim.myplugin.CustomCommand
-import me.captainpotatoaim.myplugin.custom_items.custom_fuse_tnt.CustomFuseTNT.Companion.getItem
+import me.captainpotatoaim.myplugin.custom_items.custom_fuse_tnt.CustomFuseTNT
 import me.captainpotatoaim.myplugin.util.StringHelper.camelCaseToKebabCase
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.concurrent.CompletableFuture
-import java.util.function.Consumer
 
 class GiveCustomItem(private val plugin: JavaPlugin, customItems: Array<CustomItem>) : CustomCommand {
 
-    private val customItems: MutableMap<String?, CustomItem> = HashMap()
+    private val customItems: MutableMap<String, CustomItem> = HashMap()
 
     init {
         for (item in customItems) {
@@ -33,48 +29,46 @@ class GiveCustomItem(private val plugin: JavaPlugin, customItems: Array<CustomIt
     }
 
     override fun register() {
-        val targetsParam = "targets"
-        val countParam = "count"
-        val itemParam = "item"
-        val fuseSecondsParam = "fuse-seconds"
-        val customFuseTntParam = "custom-fuse-tnt"
-
         val baseGiveCommand = CommandAPICommand("give-custom")
             .withPermission(CommandPermission.OP)
-            .withArguments(ManyPlayers(targetsParam))
+            .withArguments(EntitySelectorArgument.ManyPlayers("targets"))
 
         baseGiveCommand.copy()
-            .withArguments(StringArgument(itemParam).replaceSuggestions { sender: SuggestionInfo<CommandSender?>?, context: SuggestionsBuilder ->
-                customItems.keys.forEach(
-                    Consumer { text: String? -> context.suggest(text) })
-                CompletableFuture.completedFuture(context.build())
-            })
-            .withOptionalArguments(IntegerArgument(countParam, 1))
-            .executes(CommandExecutor { _: CommandSender?, commandArguments: CommandArguments ->
-                val targets = commandArguments.getUnchecked<Collection<Player>>(targetsParam)!!
-                val item = commandArguments.getByClass(itemParam, String::class.java)
-                val count = commandArguments.getByClassOrDefault(countParam, Int::class.java, 1)
-                val itemObj = customItems[item]
-
-                for (target in targets) {
-                    target.inventory.addItem(itemObj!!.getItem(count))
-                }
-            })
+            .withArguments(MultiLiteralArgument("item", *customItems.keys.minus("custom-fuse-tnt").toTypedArray()))
+            .withOptionalArguments(IntegerArgument("count", 1))
+            .executes(this::giveItem)
             .register(plugin)
 
         baseGiveCommand
-            .withArguments(LiteralArgument(customFuseTntParam))
-            .withArguments(FloatArgument(fuseSecondsParam, 0f))
-            .withOptionalArguments(IntegerArgument(countParam, 1))
-            .executes(CommandExecutor { _: CommandSender?, commandArguments: CommandArguments ->
-                val targets = commandArguments.getUnchecked<Collection<Player>>(targetsParam)!!
-                val count = commandArguments.getByClassOrDefault(countParam, Int::class.java, 1)
-                val fuseTime = commandArguments.getByClass(fuseSecondsParam, Float::class.java)!!
-
-                for (target in targets) {
-                    target.inventory.addItem(getItem(count, fuseTime))
-                }
-            })
+            .withArguments(LiteralArgument("custom-fuse-tnt"))
+            .withArguments(FloatArgument("fuse-seconds", 0f))
+            .withOptionalArguments(IntegerArgument("count", 1))
+            .executes(this::giveCustomFuseTnt)
             .register(plugin)
+    }
+
+    private fun giveItem(sender: CommandSender, arguments: CommandArguments) {
+        val count = arguments.getByClassOrDefault("count", Int::class.java, 1)
+        val item = arguments.getByClass("item", String::class.java)
+        val itemObj = customItems[item]
+        val items = itemObj!!.getItem(count)
+
+        this.giveItemToPlayer(items, arguments)
+    }
+
+    private fun giveCustomFuseTnt(sender: CommandSender, arguments: CommandArguments) {
+        val count = arguments.getByClassOrDefault("count", Int::class.java, 1)
+        val fuseTime = arguments.getByClass("fuse-seconds", Float::class.java)!!
+        val items = CustomFuseTNT.getItem(count, fuseTime)
+
+        this.giveItemToPlayer(items, arguments)
+    }
+
+    private fun giveItemToPlayer(item: ItemStack, arguments: CommandArguments) {
+        val targets = arguments.getUnchecked<Collection<Player>>("targets")!!
+
+        for (target in targets) {
+            target.inventory.addItem(item)
+        }
     }
 }
