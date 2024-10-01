3cbf80178b43f162f89fd76a1dc179e4a23a5a41
anviks <anviks@ttu.ee>
2024-10-01 20:15:21 +0300

    Fix false usage of Components and use Random::nextBoolean instead of Math::random, since only 2 values are needed

--- src/main/java/com/github/anviks/vixplugin/listeners/BedMessage.kt
+++ src/main/java/com/github/anviks/vixplugin/listeners/BedMessage.kt
@@ -1,9 +1,10 @@
 package com.github.anviks.vixplugin.listeners
 
-import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.Component.text
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerBedLeaveEvent
+import kotlin.random.Random
 
 class BedMessage : Listener {
 
@@ -12,13 +13,12 @@ class BedMessage : Listener {
         val player = event.getPlayer()
         val server = player.server
         if (player.playerTime == 24000L) {
-            server.broadcast(Component.text(player.displayName().toString() + " slept through the night. Thanks!"))
+            server.broadcast(player.displayName().append(text(" slept through the night. Thanks!")))
         } else {
-            val random = (Math.random() * 2).toInt()
-            when (random) {
-                0 -> player.sendMessage("Why did you wake up?")
-                1 -> player.sendMessage("Are you having trouble sleeping?")
-            }
+            player.sendMessage(
+                if (Random.nextBoolean()) "Why did you wake up?"
+                else "Are you having trouble sleeping?"
+            )
         }
     }
 }
