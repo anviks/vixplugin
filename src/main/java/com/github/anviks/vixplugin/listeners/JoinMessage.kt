afd6a3f486d1c64a54890cd28386bc6a18109169
anviks <anviks@ttu.ee>
2024-10-03 00:21:43 +0300

    Small refactor

--- src/main/java/com/github/anviks/vixplugin/listeners/JoinMessage.kt
+++ src/main/java/com/github/anviks/vixplugin/listeners/JoinMessage.kt
@@ -1,38 +1,38 @@
 package com.github.anviks.vixplugin.listeners
 
 import com.github.anviks.vixplugin.util.isVanished
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.*
-import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.NamedTextColor.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
-import org.bukkit.plugin.Plugin
-import java.util.Objects
 
-class JoinMessage(private val plugin: Plugin) : Listener {
+class JoinMessage : Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
+        var message = event.joinMessage()
+
         if (event.player.isVanished()) {
-            event.joinMessage(null)
-        } else {
-            event.joinMessage(
-                Objects.requireNonNull<Component?>(event.joinMessage()).append(text(", tell them to leave"))
-            )
+            message = null
+        } else if (message != null) {
+            message = message.append(text(", tell them to leave"))
         }
+
+        event.joinMessage(message)
     }
 
     @EventHandler
     fun onPlayerLeave(event: PlayerQuitEvent) {
+        var message = event.quitMessage()
+
         if (event.player.isVanished()) {
-            event.quitMessage(null)
-        } else {
-            event.quitMessage(
-                text("Good! ", NamedTextColor.YELLOW)
-                    .append(Objects.requireNonNull<Component?>(event.quitMessage()))
-            )
+            message = null
+        } else if (message != null) {
+            message = text("Good! ", YELLOW).append(message)
         }
+
+        event.quitMessage(message)
     }
 }
